028a4abb0eadd8b677d8c33212db93a3aa02d6b5
Александр Язь <sania.googl@gmail.com>
2026-01-17 20:56:49 +0300

    Добавьте файлы проекта.

---
+++ Model/ModPack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomLauncher.Model
+{
+    public class ModPack : INotifyPropertyChanged
+    {
+        private string title;
+        private string strings;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
