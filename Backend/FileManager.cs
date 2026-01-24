fdf3781763fc3d58f98969bffa540e7245e77020
Александр Язь <sania.googl@gmail.com>
2026-01-24 19:05:19 +0300

    Working on ModPackCrtWin

---
+++ Backend/FileManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomPacker.Backend
+{
+    public class FileManager
+    {
+        private string modsFolder;
+        public FileManager(string ModsFolder) 
+        {
+            modsFolder = ModsFolder;
+        }
+
+        public List<string> FindModsInDirectory()
+        {
+            List<string> modsList = [];
+
+            var directory = new DirectoryInfo(modsFolder);
+
+            if (directory.Exists)
+            {
+                FileInfo[] mods = directory.GetFiles();
+
+                foreach ( FileInfo mod in mods )
+                {
+                    if (mod.Name.EndsWith(".pk3") || mod.Name.EndsWith(".wad"))
+                        modsList.Add(mod.FullName);
+                }
+            }
+
+            return modsList;
+        }
+    }
+}
---
+++ ViewModel/Windows/ModPackCreationWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomPacker.ViewModel.Windows
+{
+    public class ModPackCreationWindowViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> ModsInFolder { get; set; };
+
+        public ModPackCreationWindowViewModel()
+        {
+            
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
